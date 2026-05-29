f5a30779ab6dc12c2b48783ed5defe279aa8ff94
Andrew DiZenzo <59515127+andrewtdiz@users.noreply.github.com>
2026-05-29 02:32:41 -0700

    docs: refresh async function API docs

--- docs/api/perry.d.ts
+++ docs/api/perry.d.ts
@@ -1,6 +1,6 @@
 // Auto-generated from Perry's API manifest (#465). Do not edit by hand.
 // Source: perry-api-manifest::API_MANIFEST
-// Coverage: 1501 entries across 82 modules
+// Coverage: 1506 entries across 82 modules
 
 declare module "@perryts/pdf" {
   /** stdlib */
@@ -2077,6 +2077,12 @@ declare module "util/types" {
   /** stdlib */
   export function isArrayBufferView(...args: any[]): any;
   /** stdlib */
+  export function isAsyncFunction(...args: any[]): any;
+  /** stdlib */
+  export function isBigInt64Array(...args: any[]): any;
+  /** stdlib */
+  export function isBigUint64Array(...args: any[]): any;
+  /** stdlib */
   export function isBooleanObject(...args: any[]): any;
   /** stdlib */
   export function isBoxedPrimitive(...args: any[]): any;
@@ -2086,6 +2092,10 @@ declare module "util/types" {
   export function isFloat32Array(...args: any[]): any;
   /** stdlib */
   export function isFloat64Array(...args: any[]): any;
+  /** stdlib */
+  export function isGeneratorFunction(...args: any[]): any;
+  /** stdlib */
+  export function isGeneratorObject(...args: any[]): any;
   /** stdlib */
   export function isInt16Array(...args: any[]): any;
   /** stdlib */
